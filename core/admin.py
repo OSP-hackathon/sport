19f7586a8f97cf35e3ba909166c128e55798041c
Grisha Rybolovlev <rybolovlev.grisha@gmail.com>
2023-04-27 16:14:53 +0300

    Model added

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class SiteUserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'role')
+    search_fields = ('id',)
+
+
+admin.site.register(SiteUser, SiteUserAdmin)
--- core/models.py
+++ core/models.py
@@ -1,7 +1,40 @@
 from django.db import models
 from django.conf import settings
+from django.contrib.postgres.fields import ArrayField
 
-# Create your models here.
 
-class SiteUser:
+class SiteUser(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.deletion.CASCADE, null=True)
+    name = models.CharField(max_length=63, blank=True, verbose_name='Имя')
+    surname = models.CharField(max_length=63, blank=True, verbose_name='Фамилия')
+    last_name = models.CharField(max_length=63, blank=True, verbose_name='Отчество')
+    COMP_CHOICES = [
+        ('BACK', 'Backend'),
+        ('FRON', 'Frontend'),
+    ]
+    competence = models.CharField(max_length=4,
+                            choices=COMP_CHOICES,
+                            default='BACK',
+                            verbose_name='Компетенция',
+                            blank=True)
+    rating = models.IntegerField(blank=True, null=True, verbose_name='Рейтинг')
+    awards = models.TextField(blank=True, null=True, verbose_name='Достижения')
+    skills = ArrayField(base_field = ArrayField(models.CharField(blank=True),),
+                        blank=True,
+                        null=True,
+                        verbose_name='Навыки')
+    ROLE_IN_SYSTEM_CHOICES = [
+        ('SP', 'Спортсмен'),
+        ('RE', 'Представитель региональной федерации'),
+        ('AD', 'Администратор ФСП'),
+        ('PA', 'Партнер федерации')
+    ]
+    role = models.CharField(max_length=2,
+                            choices=ROLE_IN_SYSTEM_CHOICES,
+                            default='SP',
+                            verbose_name='Роль',
+                            blank=True)
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
