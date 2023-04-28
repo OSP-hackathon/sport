2ad65b36e38b9c6247e45c97f1d035aa216a3f3d
p50000 <rentacat45@gmail.com>
2023-04-28 10:34:43 +0300

    add calendar

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,5 @@
 from django.urls import path
-from django.urls import re_path
-from django.conf.urls.static import static
-from django.views.static import serve
 
-from vhack.settings import *
-from vhack import settings
 from .views import *
 
 urlpatterns = [
@@ -12,7 +7,7 @@ urlpatterns = [
      path('login', loginPage, name='login'),
      path('register', registerPage, name='register'),
      path('profile', profilePage, name='profile'),
-     path('logout', logoutUser, name='logoutUser')
+     path('logout', logoutUser, name='logoutUser'),
      path('profile', profilePage, name='profile'),
      path('api/login', loginApi, name='loginApi')
 ]
